77d7ca16462f7e3026e4e5518a42a4cb6013572a
Ik4ri-prog <ik4ri.dump@gmail.com>
2026-04-30 14:14:33 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Register from "./components/RegisterForm";
 import Dashboard from "./pages/Dashboard";
 import UserApproval from "./pages/UserApproval";
 import EmployeeRecords from "./pages/EmployeeRecords";
+import AuditLog from "./pages/AuditLog";
+import UserEmployee from "./pages/UserEmployee";
+
+
 
 function App() {
   return (
@@ -13,9 +17,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/useremployee" element={<UserEmployee />} />
         <Route path="/userapproval" element={<UserApproval />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/employeerecords" element={<EmployeeRecords />} />
+        <Route path="/auditlog" element={<AuditLog />} />
+
       </Routes>
     </Router>
   );
